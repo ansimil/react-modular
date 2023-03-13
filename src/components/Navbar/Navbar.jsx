42aff26ad31cc26f7f895e3e0f706b28ac29c15e
John Doe <andrewsimonmiller@gmail.com>
2023-03-13 19:45:36 +1100

    implement a navbar-btn component for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { handleMouseEvent } from '../../services/general.services'
+import React from 'react'
 import './Navbar.css'
 
 const Navbar = ({seqRef, oscillatorsRef, lfosRef, filterRef, envelopeRef, oscilloscopeRef, matrixLocationRef, effectsLocationRef, keysRef, vcaRef}) => {
@@ -13,142 +14,37 @@ const Navbar = ({seqRef, oscillatorsRef, lfosRef, filterRef, envelopeRef, oscill
         })
     }
 
+    const NavbarBtn = React.forwardRef( (props, ref) => {
+        return (
+        <button 
+        className={`btn navbar-btn ${props.name}-navbar-btn`}
+        onClick={()=>{
+            scrollTo(ref)}
+        }
+        onMouseDown={()=>{
+            handleMouseEvent(`${props.name}-navbar-btn`, true)
+        }}
+        onMouseUp={()=>{
+            handleMouseEvent(`${props.name}-navbar-btn`, false)
+        }}
+        >
+        {`_${props.name}`}
+        </button>
+        )
+    })
+
   return (
     <div className="navbar-container">
         <p className="navbar-title">navigation:</p>
-        <button 
-        className="btn navbar-btn matrix-navbar-btn" 
-        onClick={()=>{
-            scrollTo(matrixLocationRef)}
-        }
-        onMouseDown={()=>{
-            handleMouseEvent("matrix-navbar-btn", true)
-        }}
-        onMouseUp={()=>{
-            handleMouseEvent("matrix-navbar-btn", false)
-        }}
-        >
-        _matrix
-        </button>
-
-        <button 
-        className="btn navbar-btn seq-navbar-btn" 
-        onClick={
-            ()=>{scrollTo(seqRef)}
-        }
-        onMouseDown={()=>{
-            handleMouseEvent("seq-navbar-btn", true)
-        }}
-        onMouseUp={()=>{
-            handleMouseEvent("seq-navbar-btn", false)
-        }}
-        >
-        _seq
-        </button>
-
-        <button 
-        className="btn navbar-btn oscillators-navbar-btn" 
-        onClick={
-            ()=>{scrollTo(oscillatorsRef)}
-        }
-        onMouseDown={()=>{
-            handleMouseEvent("oscillators-navbar-btn", true)
-        }}
-        onMouseUp={()=>{
-            handleMouseEvent("oscillators-navbar-btn", false)
-        }}
-        >
-        _oscillators
-        </button>
-
-        <button 
-        className="btn navbar-btn lfos-navbar-btn" 
-        onClick={
-            ()=>{scrollTo(lfosRef)}
-        }
-        onMouseDown={()=>{
-            handleMouseEvent("lfos-navbar-btn", true)
-        }}
-        onMouseUp={()=>{
-            handleMouseEvent("lfos-navbar-btn", false)
-        }}
-        >
-        _lfos
-        </button>
-
-        <button 
-        className="btn navbar-btn filter-navbar-btn" 
-        onClick={
-            ()=>{scrollTo(filterRef)}
-        }
-        onMouseDown={()=>{
-            handleMouseEvent("filter-navbar-btn", true)
-        }}
-        onMouseUp={()=>{
-            handleMouseEvent("filter-navbar-btn", false)
-        }}
-        >
-        _filter
-        </button>
-
-        <button 
-        className="btn navbar-btn envelope-navbar-btn" 
-        onClick={
-            ()=>{scrollTo(envelopeRef)}
-        }
-        onMouseDown={()=>{
-            handleMouseEvent("envelope-navbar-btn", true)
-        }}
-        onMouseUp={()=>{
-            handleMouseEvent("envelope-navbar-btn", false)
-        }}
-        >
-        _envelope
-        </button>
-
-        <button 
-        className="btn navbar-btn effects-navbar-btn" 
-        onClick={
-            ()=>{scrollTo(effectsLocationRef)}
-        }
-        onMouseDown={()=>{
-            handleMouseEvent("effects-navbar-btn", true)
-        }}
-        onMouseUp={()=>{
-            handleMouseEvent("effects-navbar-btn", false)
-        }}
-        >
-        _effects
-        </button>
-
-        <button 
-        className="btn navbar-btn oscilloscope-navbar-btn" 
-        onClick={
-            ()=>{scrollTo(oscilloscopeRef)}
-        }
-        onMouseDown={()=>{
-            handleMouseEvent("oscilloscope-navbar-btn", true)
-        }}
-        onMouseUp={()=>{
-            handleMouseEvent("oscilloscope-navbar-btn", false)
-        }}
-        >
-        _oscilloscope
-        </button>
-        <button 
-        className="btn navbar-btn keys-navbar-btn" 
-        onClick={
-            ()=>{scrollTo(keysRef)}
-        }
-        onMouseDown={()=>{
-            handleMouseEvent("keys-navbar-btn", true)
-        }}
-        onMouseUp={()=>{
-            handleMouseEvent("keys-navbar-btn", false)
-        }}
-        >
-        _keys
-        </button>
+        <NavbarBtn name="matrix" ref={matrixLocationRef} />
+        <NavbarBtn name="seq" ref={seqRef} />
+        <NavbarBtn name="oscillators" ref={oscillatorsRef} />
+        <NavbarBtn name="lfos" ref={lfosRef} />
+        <NavbarBtn name="filter" ref={filterRef} />
+        <NavbarBtn name="envelopes" ref={envelopeRef} />
+        <NavbarBtn name="effects" ref={effectsLocationRef} />
+        <NavbarBtn name="oscilloscope" ref={oscilloscopeRef} />
+        <NavbarBtn name="keys" ref={keysRef} />
     </div>
   )
 }
